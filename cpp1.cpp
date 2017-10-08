f941bb0113c5804b20dfc3a29eb101ea1afb2e75
Şükrü Görkem Okur <a01001474@unet.univie.ac.at>
2017-10-08 15:11:37 +0200

    write to screen HelloWorld

---
+++ cpp1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	// this code write to screen Hello World.
+	// cout used for write to screen something
+	cout<<"Hello World";
+	
+	// \n = new line
+	cout<<'\n';
+
+	//for win7
+	system("PAUSE");
+
+	return 0;
+}
